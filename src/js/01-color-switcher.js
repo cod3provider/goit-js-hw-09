e1b291256de3f544ee2a15ddf35eed82a024700f
Ilya Bondarchuk <89864785+cod3provider@users.noreply.github.com>
2022-09-12 07:44:40 +0300

    task 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+const backgroundSetter = {
+  isActive: false,
+  intervalId: null,
+  start() {
+    if (this.isActive) {
+      return;
+    };
+
+    this.intervalId = setInterval(() => {
+      this.isActive = true;
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+  },
+  stop() {
+    this.isActive = false;
+    clearInterval(this.intervalId);
+  },
+}
+
+refs.startBtn.addEventListener('click', startSetter);
+refs.stopBtn.addEventListener('click', stopSetter);
+
+function startSetter() {
+  backgroundSetter.start();
+}
+
+function stopSetter() {
+  backgroundSetter.stop();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
